0d2c995492728c542bce30cda5a884a268b69e3e
yungtravla <lilprismontour@gmail.com>
2018-06-02 19:06:16 +1000

    accuracy

--- youtube.com/mute-ads.js
+++ youtube.com/mute-ads.js
@@ -4,6 +4,7 @@ setInterval(function(){
     ( 
       document.querySelector("div#player.ytd-watch").innerHTML.match(/your video will begin in/i) 
       || document.querySelector("div#player.ytd-watch").innerHTML.match(/you can skip to video in/i) 
+      || document.querySelector("div#player.ytd-watch").innerHTML.match(/video will play after ad/i) 
     ) 
     && !yungtravla_muted 
   ) {
@@ -13,6 +14,7 @@ setInterval(function(){
     !( 
       document.querySelector("div#player.ytd-watch").innerHTML.match(/your video will begin in/i) 
       || document.querySelector("div#player.ytd-watch").innerHTML.match(/you can skip to video in/i) 
+      || document.querySelector("div#player.ytd-watch").innerHTML.match(/video will play after ad/i) 
     ) 
     && yungtravla_muted 
   ) {
