3af3dbc8c284339e817d4da038497999f3c0fc68
yungtravla <lilprismontour@gmail.com>
2018-06-02 19:45:53 +1000

    close pop-up ads

--- youtube.com/mute-ads.js
+++ youtube.com/mute-ads.js
@@ -1,24 +1,29 @@
 var yungtravla_muted = false
 setInterval(function(){
-  if ( 
-    ( 
-      document.querySelector("div#player.ytd-watch").innerHTML.match(/your video will begin in/i) 
-      || document.querySelector("div#player.ytd-watch").innerHTML.match(/you can skip to video in/i) 
-      || document.querySelector("div#player.ytd-watch").innerHTML.match(/video will play after ad/i) 
+	// Mute ad
+	if ( 
+		( 
+			document.querySelector("div#player.ytd-watch").innerText.match(/video will begin in/ig) 
+			|| document.querySelector("div#player.ytd-watch").innerText.match(/can skip to video in/ig) 
+			|| document.querySelector("div#player.ytd-watch").innerText.match(/video will play after ad/ig) 
     ) 
     && !yungtravla_muted 
   ) {
     document.querySelector("button.ytp-mute-button.ytp-button").click()
     yungtravla_muted = true
-  } else if (
+  } else if ( 
     !( 
-      document.querySelector("div#player.ytd-watch").innerHTML.match(/your video will begin in/i) 
-      || document.querySelector("div#player.ytd-watch").innerHTML.match(/you can skip to video in/i) 
-      || document.querySelector("div#player.ytd-watch").innerHTML.match(/video will play after ad/i) 
+			document.querySelector("div#player.ytd-watch").innerText.match(/video will begin in/ig) 
+			|| document.querySelector("div#player.ytd-watch").innerText.match(/can skip to video in/ig) 
+			|| document.querySelector("div#player.ytd-watch").innerText.match(/video will play after ad/ig) 
     ) 
     && yungtravla_muted 
   ) {
     document.querySelector("button.ytp-mute-button.ytp-button").click()
     yungtravla_muted = false
   }
-}, 420)
+	// Close pop-up ads
+	if ( document.querySelectorAll("div.overlays-action-tray div.close-padding").length > 0 ) {
+		document.querySelector("div.overlays-action-tray div.close-padding").click()
+	}
+}, 200)
