f558f9febb6ad9c269c6be54793a929473c0937d
yungtravla <lilprismontour@gmail.com>
2018-06-13 16:50:39 +1000

    Update ads.js

--- youtube.com/ads.js
+++ youtube.com/ads.js
@@ -1,36 +1,47 @@
-var yungtravla_muted = false
-yungtravla_loop = setInterval(function(){
-	// Automatically skip ads
-	if ( document.querySelector("div#player.ytd-watch").innerText.match(/Skip/g) ) {
-		document.querySelectorAll("div#player.ytd-watch div").forEach(function(div){
-			div.innerText == "Skip Ad" ? div.click() : ""
-			div.innerText == "Skip to End" ? div.click() : ""
-		})
-	}
-	// Mute ads
-	if(
-			document.querySelector("div#player.ytd-watch").innerText.match(/video will begin/ig) 
-			|| document.querySelector("div#player.ytd-watch").innerText.match(/you can skip to video/ig) 
-			|| document.querySelector("div#player.ytd-watch").innerText.match(/video will play after ad/ig) 
-	) {
-		if (!yungtravla_muted) {
-			document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
-			yungtravla_muted = true
-		}
-	} else {
-		if (yungtravla_muted) {
-			document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
-			yungtravla_muted = false
-		}
-	}
-	// Close/remove other ads
-	if ( document.querySelectorAll("div.overlays-action-tray div.close-padding").length > 0 ) {
-		document.querySelector("div.overlays-action-tray div.close-padding").click()
-	}
-	document.querySelectorAll("div.annotation").forEach(function(annotation){
-		annotation.remove()
-	})
-	document.querySelectorAll("div#player-ads").forEach(function(ad){
-		ad.remove()
-	})
-}, 666)
+let yungtravla_hide_annotations = true,
+    yungtravla_muted = false,
+    yungtravla_loop = setInterval(function(){
+    	if ( document.querySelector("div#player.ytd-watch") ) {
+    
+    		// Skip ads
+    		if ( document.querySelector("div#player.ytd-watch").innerText.match(/Skip/g) ) {
+    			document.querySelectorAll("div#player.ytd-watch div").forEach(function(div){
+    				div.innerText == "Skip Ad" ? div.click() : ""
+    				div.innerText == "Skip to End" ? div.click() : ""
+    			})
+    		}
+    
+    		// Mute ads
+    		if (
+    			document.querySelector("div#player.ytd-watch").innerText.match(/video will begin/ig) 
+    			|| document.querySelector("div#player.ytd-watch").innerText.match(/you can skip to video/ig) 
+    			|| document.querySelector("div#player.ytd-watch").innerText.match(/video will play after ad/ig) 
+    		) {
+    			if (!yungtravla_muted) {
+    				document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
+    				yungtravla_muted = true
+    			}
+    		} else {
+    			if (yungtravla_muted) {
+    				document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
+    				yungtravla_muted = false
+    			}
+    		}
+    
+    		// Close/remove other ads
+    		document.querySelectorAll("div#player.ytd-watch div.close-padding").forEach(function(close){
+    			close.click()
+    		})
+    		document.querySelectorAll("div#player-ads").forEach(function(ad){
+    			ad.remove()
+    		})
+    
+    		// Hide annotations if required
+    		if ( yungtravla_hide_annotations ) {
+    			document.querySelectorAll("div.annotation").forEach(function(annotation){
+    				annotation.remove()
+    			})
+    		}
+    
+    	}
+    }, 666)
