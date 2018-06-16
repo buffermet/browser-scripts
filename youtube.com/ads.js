3646e567aa6275410452105d22ac221f37f283ba
yungtravla <lilprismontour@gmail.com>
2018-06-16 12:03:23 +1000

    Update ads.js

--- youtube.com/ads.js
+++ youtube.com/ads.js
@@ -1,34 +1,35 @@
+// I have more time than an Arctic Whale
 let yungtravla_hide_annotations = true,
     yungtravla_muted = false,
     yungtravla_loop = setInterval(function(){
-    	if ( document.querySelector("div#player.ytd-watch") ) {
+    	if ( document.querySelector("div#player-container") ) {
     
     		// Skip ads
-    		if ( document.querySelector("div#player.ytd-watch").innerText.match(/skip\s(ad|to\send)/ig) ) {
-    			document.querySelectorAll("div#player.ytd-watch div").forEach(function(div){
-    				div.innerText == "Skip Ad" ? div.click() : ""
-    				div.innerText == "Skip to End" ? div.click() : ""
+    		if ( document.querySelector("div#player-container").innerText.match(/skip\s(ad|to\send)/ig) ) {
+    			document.querySelectorAll("div#player-container div").forEach(function(div){
+    				div.innerText.match(/^Skip\s*Ad$/i) ? div.click() : ""
+    				div.innerText.match(/^Skip\s*to\s*End$/i) ? div.click() : ""
     			})
     		} else {
     			// Mute ads
     			if (
-    				document.querySelector("div#player.ytd-watch").innerText.match(/video\swill\sbegin/ig) 
-    				|| document.querySelector("div#player.ytd-watch").innerText.match(/you\scan\sskip\sto\svideo/ig) 
-    				|| document.querySelector("div#player.ytd-watch").innerText.match(/video\swill\splay\safter\sad/ig) 
+    				document.querySelector("div#player-container").innerText.match(/video\s*will\s*begin/ig) 
+    				|| document.querySelector("div#player-container").innerText.match(/you\s*can\s*skip\s*to\s*video/ig) 
+    				|| document.querySelector("div#player-container").innerText.match(/video\s*will\s*play\s*after\s*ad/ig) 
     			) {
     				if (!yungtravla_muted) {
-    					document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
+    					document.querySelector("div#player-container button.ytp-mute-button.ytp-button").click()
     					yungtravla_muted = true
     				}
     			} else {
     				if (yungtravla_muted) {
-    					document.querySelector("div#player.ytd-watch button.ytp-mute-button.ytp-button").click()
+    					document.querySelector("div#player-container button.ytp-mute-button.ytp-button").click()
     					yungtravla_muted = false
     				}
     			}
     
     			// Close/remove other ads
-    			document.querySelectorAll("div#player.ytd-watch div.close-padding").forEach(function(close){
+    			document.querySelectorAll("div#player-container div.close-padding").forEach(function(close){
     				close.click()
     			})
     			document.querySelectorAll("div#player-ads").forEach(function(ad){
