4e18523b2ccd9e184723736dacf90fc1292a730e
yungtravla <lilprismontour@gmail.com>
2018-06-30 12:58:11 +1000

    Create ads.js

---
+++ soundcloud.com/ads.js
@@ -0,0 +1,11 @@
+let yungtravla_muted = false
+const yungtravla_loop = setInterval(()=>{
+	// Mute ads
+	if ( !yungtravla_muted && document.querySelector(".playControlsPanel.is-visible") ) {
+		document.querySelector(".volume__button").click()
+		yungtravla_muted = true
+	} else if ( yungtravla_muted && !document.querySelector(".playControlsPanel.is-visible") ) {
+		document.querySelector(".volume__button").click()
+		yungtravla_muted = false
+	}
+}, 666)
